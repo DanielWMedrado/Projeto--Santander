6e1974a3ee0b793574a941803df40cb518500895
Daniel Medrado <dwsmqueiros@outlook.com>
2024-03-19 19:17:22 -0300

    2.5

--- assets/js/index/header.js
+++ assets/js/index/header.js
@@ -1,5 +1,7 @@
 let inputsearch = document.getElementById('inputserch')
 let searchbar = document.getElementById('head-searchbar')
+let prevScrollPos = window.pageYOffset;
+
 
 
 //Verica se o usuario clicou no campo de pesquisa e dispara o evento focus que vai selecionar automaticamente a partir do evento o campo desejado que nesse caso é a barra de pdsquisa
@@ -21,3 +23,17 @@ document.addEventListener('click', function(event) {
         searchbar.style.transform = 'scale(1)';
         }
     })
+
+    
+
+window.addEventListener('scroll', function() {
+    let currentScrollPos = window.pageYOffset;
+
+    // Verifica se a rolagem está para baixo
+    if (prevScrollPos > currentScrollPos) {
+        document.getElementById('header').style.top = '0'; // Exibe o cabeçalho
+    } else {
+        document.getElementById('header').style.top = '-100px'; // Esconde o cabeçalho
+    }
+    prevScrollPos = currentScrollPos;
+});
