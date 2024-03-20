f21af6c3de71c46ea9300e6c55582020e3831fed
Daniel Medrado <dwsmqueiros@outlook.com>
2024-03-20 13:20:42 -0300

    Update index

--- assets/js/index/header.js
+++ assets/js/index/header.js
@@ -1,19 +1,34 @@
-let inputsearch = document.getElementById('inputserch')
+let inputsearch = document.getElementById('inputsearch')
 let searchbar = document.getElementById('head-searchbar')
-let prevScrollPos = window.pageYOffset;
+let iconsearch = document.getElementById('iconsearch')
+let iconsearch2 = document.getElementById('iconsearch2')
+let iconreturn = document.getElementById('iconreturn')
 
 
 
 //Verica se o usuario clicou no campo de pesquisa e dispara o evento focus que vai selecionar automaticamente a partir do evento o campo desejado que nesse caso é a barra de pdsquisa
-searchbar.addEventListener("click", ()=>{
-    searchbar.classList.toggle('searching')
-
+inputsearch.addEventListener("click", ()=>{
+    iconsearch.classList.add('hidden');
+    iconreturn.classList.remove('hidden');
+    iconsearch2.classList.remove('hidden2');
     inputsearch.focus();
-    searchbar.style.transform = 'scale(1.2)';
-    searchbar.style.boxShadow = '0px 1px 1px 0px rgba(0, 0, 0, 0.219)';
+    searchbar.style.boxShadow = '0px 1.5px 3px 1px rgba(0, 0, 0, 0.219)';
 })
 
+iconsearch.addEventListener("click", ()=>{
+    iconsearch.classList.add('hidden')
+    iconreturn.classList.remove('hidden')
+    inputsearch.focus();
+    searchbar.style.boxShadow = '0px 1px 1px 1px rgba(0, 0, 0, 0.219)';
+})
 
+iconreturn.addEventListener('click', ()=>{
+    iconsearch.classList.remove('hidden');
+    iconreturn.classList.add('hidden');
+    iconsearch2.classList.add('hidden2');
+    searchbar.style.boxShadow = 'none';
+    
+})
 
 // Adicione um event listener para cliques no documento
 document.addEventListener('click', function(event) {
@@ -21,7 +36,11 @@ document.addEventListener('click', function(event) {
     // Verifique se o clique ocorreu fora do campo desejado
     if (!searchbar.contains(event.target)) {
         // O clique ocorreu fora do campo desejado, execute a lógica desejada aqui
-        searchbar.style.transform = 'scale(1)';
+        iconsearch.classList.remove('hidden');
+        iconreturn.classList.add('hidden')
+        iconsearch2.classList.add('hidden2');
+        searchbar.style.boxShadow = 'none';
+
         }
     })
 
